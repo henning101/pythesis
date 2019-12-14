897e661ccb57b82e0728db6ff6e09b108c93c1b3
Henning Metzmacher <henning.metzmacher@gmail.com>
2019-12-14 12:04:29 +0100

    Add web app build to the repository
    - Add web app build to the repository
    - Change respective section in the example PDF

---
+++ app/build/precache-manifest.fe0430369ba8382c3e37d49d56993085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1df471cd5517f6195387b5f8c46589a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb77020632d7aeda9b86",
+    "url": "/static/css/main.57ca44f5.chunk.css"
+  },
+  {
+    "revision": "4df0bcec87344706413f",
+    "url": "/static/js/2.317d5cc1.chunk.js"
+  },
+  {
+    "revision": "cb77020632d7aeda9b86",
+    "url": "/static/js/main.ff9513a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "52e316e74932a5884c34c903b06143a5",
+    "url": "/static/media/loading.52e316e7.svg"
+  }
+]);
