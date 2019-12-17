8aa9b4a23c683971a63f5c40deb09f8ecccae0a9
Henning Metzmacher <henning.metzmacher@gmail.com>
2019-12-17 18:42:17 +0100

    Start MATLAB in start_example_with_matlab.bat
    - Start MATLAB and share the engine in start_example_with_matlab.bat

--- start_example_with_matlab.bat
+++ start_example_with_matlab.bat
@@ -1,1 +1,4 @@
+REM Run MATLAB and share the engine:
+matlab -r "matlab.engine.shareEngine"
+REM Run PyThesis with matlab flag:
 python start.py --project_root %~dp0/example --main_document example --matlab
