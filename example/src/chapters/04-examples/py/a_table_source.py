ba65254b9e885ca9cfb4c0d6ba43139e235b83fa
Henning Metzmacher <henning.metzmacher@gmail.com>
2020-02-12 14:17:33 +0100

    Fix the table documentation

--- example/src/chapters/04-examples/py/a_table_source.py
+++ example/src/chapters/04-examples/py/a_table_source.py
@@ -1,3 +1,3 @@
 # Directly open the source file and print the content:
-with open(os.path.abspath('./a_table_source.py')) as f:
+with open(os.path.abspath('./a_table.py')) as f:
     print(f.read())
